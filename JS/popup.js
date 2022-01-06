38b782243a629fe54141b2ed17e42048ca14ed4d
Zeshan Ayub <unofficialmz@gmail.com>
2022-01-06 11:54:29 +0500

    Created_PopJS

---
+++ JS/popup.js
@@ -0,0 +1,36 @@
+inp1.addEventListener("click", async() => {
+    let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+
+    chrome.scripting.executeScript({
+        target: { tabId: tab.id },
+        function: markRadio,
+    });
+});
+inp2.addEventListener("click", async() => {
+    let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+
+    chrome.scripting.executeScript({
+        target: { tabId: tab.id },
+        function: markCheckBox,
+    });
+});
+
+function markCheckBox() {
+    var ch = document.getElementsByTagName('input');
+    for (var i = 0; i < ch.length; i++) {
+        if (ch[i].type == 'checkbox') {
+            ch[i].checked = true;
+        }
+
+    }
+};
+
+function markRadio() {
+    var ch = document.getElementsByTagName('input');
+    for (var i = 0; i < ch.length; i++) {
+        if (ch[i].type == 'radio') {
+            ch[i].checked = true;
+        }
+
+    }
+};
